9732525d1dff1543f5e09effbb86fc50d7ea0873
Emerson Villalta <reaperfocs@gmail.com>
2023-04-19 18:00:58 -0500

    prueba reporte

---
+++ db/models/trabajadorCapacitacion.model.js
@@ -0,0 +1,4 @@
+const {Model, DataTypes, Sequelize} = require('sequelize');
+
+const {TRABAJADOR_TABLE} = require('./trabajador.model');
+const { CAPACITACION_TABLE } = require('./capacitacion.model');
