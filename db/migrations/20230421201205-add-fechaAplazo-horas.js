aa2188338a0d3d110cce85dacf9b38af225f5b9f
Emerson Villalta <reaperfocs@gmail.com>
2023-04-21 15:45:19 -0500

    railway docker

---
+++ db/migrations/20230421201205-add-fechaAplazo-horas.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const { CAPACITACION_TABLE, CapacitacionSchema } = require('../models/capacitacion.model');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(CAPACITACION_TABLE, 'fechaAplazo', CapacitacionSchema.fechaAplazo);
+    await queryInterface.addColumn(CAPACITACION_TABLE, 'horas', CapacitacionSchema.horas);
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    
+  }
+};
