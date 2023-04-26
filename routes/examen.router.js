082342d601d540e4a196dfea6e3eccad2ed7b684
Emerson Villalta <reaperfocs@gmail.com>
2023-04-26 10:08:22 -0500

    ruta pregunta cambiada

--- routes/examen.router.js
+++ routes/examen.router.js
@@ -24,6 +24,25 @@ router.get('/preguntas', async (req,res, next)=>{
     }
 })
 
+router.patch('/preguntas/:id', async(req,res)=>{
+    try {
+        const {id } = req.params;
+        const body = req.body;
+        const pregunta = await models.Pregunta.findByPk(id);
+        console.log(pregunta);
+        if(!pregunta){
+            res.status(404).json({message:"Pregunta no encontrada"})
+        }else{
+            console.log('llegamos');
+            console.log(pregunta);
+            const respuesta = await pregunta.update(body);
+            res.status(200).json(respuesta)
+        }
+    } catch (error) {
+        res.status(500).json({message: 'Pregunta no fue actualizada'})
+    }
+})
+
 router.get('/:id', async(req,res, next)=>{
     try {
         const id = req.params.id;
@@ -38,6 +57,7 @@ router.get('/:id', async(req,res, next)=>{
         next(error)        
     }
 })
+
 
 
 module.exports = router
