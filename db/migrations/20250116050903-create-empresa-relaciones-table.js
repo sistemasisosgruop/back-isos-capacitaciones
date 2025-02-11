fa2dd259141f6953d75f4e0a94bb88507a205ec6
Cristhian Ocola <ocolapintoc@gmail.com>
2025-02-11 12:53:56 -0500

    Fix:migrattion

--- db/migrations/20250116050903-create-empresa-relaciones-table.js
+++ db/migrations/20250116050903-create-empresa-relaciones-table.js
@@ -1,10 +1,10 @@
 'use strict';
-const { EmpresaTrabajadorSchema, EMPRESA_RELACIONES_TABLE } = require('../models/empresaRelaciones.model');
+const { EmpresaRelacionesSchema, EMPRESA_RELACIONES_TABLE } = require('../models/empresaRelaciones.model');
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable(EMPRESA_RELACIONES_TABLE, EmpresaTrabajadorSchema);
+    await queryInterface.createTable(EMPRESA_RELACIONES_TABLE, EmpresaRelacionesSchema);
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable(EMPRESA_RELACIONES_TABLE);
